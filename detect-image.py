1a0b99d5c31fa05f942cec876d8386638b1702d2
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-26 21:19:42 +0900

    이미지 가져오는 내용 추가

--- detect-image.py
+++ detect-image.py
@@ -4,10 +4,13 @@ An Example for detecting image responses using Content-Type from HTTP headers.
 Run as follows: mitmproxy -s detect-image.py
 """
 
+
 class ImageInterceptor:
     def response(self, flow):
         content_type: str = flow.response.headers.get(b'Content-Type')
         if 'image' in content_type:
+            image = flow.response.content
+            # classification logics
             flow.kill()
 
 
