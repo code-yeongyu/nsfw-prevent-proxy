06ac8f51d4234e898c351edac9119e98a505a25d
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-08-18 18:18:20 +0900

    Add spend time logging

--- detect-image.py
+++ detect-image.py
@@ -4,11 +4,12 @@ An Example for detecting image responses using Content-Type from HTTP headers.
 Run as follows: mitmdump -s detect-image.py
 """
 
-import requests
+import time, requests
 
 
 class ImageInterceptor:
-    async def response(self, flow):
+    def response(self, flow):
+        start = time.time()  # 시작 시간 저장
         content_type: str = flow.response.headers.get(b'Content-Type')
         if content_type is not None and 'image' in content_type:  # detect image
             image = flow.response.content
@@ -17,6 +18,7 @@ class ImageInterceptor:
                 files={'image': image},
             )
             flow.response.content = response.content
+            print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
 
 
 addons = [ImageInterceptor()]
